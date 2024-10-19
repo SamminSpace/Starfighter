31a0bfaa2470fbb67cdab3cb1602e05b762ebff0
Sam <spl0005@uah.edu>
2024-10-19 18:44:44 -0500

    Incorporated readings into telemetry loop
    Signed-off-by: Sam <spl0005@uah.edu>

--- BMP388.ino
+++ BMP388.ino
@@ -1,8 +1,37 @@
-// #include <SD.h> //SD library
-#include <Wire.h> //I2C
-#include <SPI.h> //OLED
-#include <107-Arduino-BMP388.h> // the actual BMP library
+#include <SD.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <107-Arduino-BMP388.h>
 #include "Adafruit_BMP3XX.h"
+
+//TELEMETRY 
+String TeamID = "Starfighter";
+String payload_state = "launch";
+float MissionTime = 0; 
+float UTCtime = 0; 
+int Packetnum = 0;
+
+
+//variables
+float temperature = 0;
+float pressure = 0;
+float altitude = 0; //Rori is right :(
+float accel_x = 0;
+float accel_y = 0;
+float accel_z = 0;
+float gyro_x = 0;
+float gyro_y = 0;
+float gyro_z = 0;
+float gps_lat = 0;
+float gps_long = 0;
+float gps_alt = 0;
+float velocity = 0;
+
+
+//set up the pins
+const int chipSelect = 10;
+int BMP_sensor = 19;
+int output_pin = 18;
 
 //BMP3XX
 #define BMP_SCK 13
@@ -12,67 +41,89 @@
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_BMP3XX bmp;
 
+//File 
+File dataFile;
 
 
-// all pin set ups
-int BMP_sensor = 19;
-int output_pin = 18;
-
-
-
-void setup() {
-  Serial.begin(9600);
-  Serial1.begin(9600); //check buad rate
+void setup()
+{
   pinMode(BMP_sensor, INPUT);
   pinMode(output_pin, OUTPUT);
-
-  if (!bmp.begin_I2C()) {  // this sets up I2C
-    Serial.println("Could not find BMP3 sensor."); 
-    while (1);
+  Serial.begin(9600); //this might need to change 152500 or whtvr
+  while (!Serial) {
+    ; // wait for serial port to connect.
   }
 
-}
 
-
-void writeFile() {  //writes to SD Card
-  Serial1.print("Tempertaure: ");
-  Serial1.print(bmp.temperature);
-  Serial1.println(" *C");
-
-  Serial1.print("Pressure: ");
-  Serial1.print(bmp.pressure / 100.0);
-  Serial1.println(" hPa");
-
-  Serial1.print("Aprox Altitude: ");
-  Serial1.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial1.println(" m");
+  Serial.print("Initializing SD card...");
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    while (1) {
+      // No SD card, so don't do anything more - stay stuck here
+    }
   }
-}
+  Serial.println("card initialized.");
 
-void loop() {
-/* if (!bmp.performReading()) {
-   Serial.println("Failed to read.");
-   return;
- } */
+
+  if (!bmp.begin_I2C()) {
+    // this sets up I2C
+    Serial.println("Could not find BMP3 sensor.");
+     while (1); 
+  }
+
+  // Set up oversampling and filter initialization (tbh IDK)
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
   
-  Serial.print("Tempertaure: ");
+}
+
+
+
+
+void loop()
+{
+  Serial.print("Temperature: ");
   Serial.print(bmp.temperature);
   Serial.println(" *C");
+  temperature = bmp.temperature; 
 
   Serial.print("Pressure: ");
   Serial.print(bmp.pressure / 100.0);
   Serial.println(" hPa");
+  pressure = bmp.pressure; 
 
   Serial.print("Aprox Altitude: ");
-  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA);
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
   Serial.println(" m");
+  altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA);
 
   Serial.println();
-  /* make sure after the test that the format matches telemetry
-  this is for testing to make sure it works */
 
-  writeFile();
-  delay(3000);
+ TelemertyLoop();
+ delay(1000);
 }
+
+
+
+
+void TelemertyLoop ()
+{
+  MissionTime = (millis() / 1000); //get time before send into telemtry
+
+  File dataFile = SD.open("BMP.txt", FILE_WRITE);
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(TeamID + ", " + MissionTime + ", " + UTCtime + 
+    ", " + Packetnum + ", " + payload_state + ", " + altitude + ", "
+    + temperature + ", " + pressure);
+    dataFile.close();
+  }
+
+  Packetnum = Packetnum + 1;
+}
+
 
 
